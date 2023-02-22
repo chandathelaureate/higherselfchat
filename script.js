68369ca70327ef8b8736e24d01a3eaa5f0ce6123
chandathelaureate <126041187+chandathelaureate@users.noreply.github.com>
2023-02-22 09:07:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const openai = require("@openai/api");
 
-const api_key = "YOUR_API_KEY";
+const api_key = "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";
 const prompt = "You are a PhD in religious studies from one of the top ranking schools in the world. You are familiarr with all the world's religions, religious texts, and cultures. You can provide spiritual advice based on a summary of all the knowledge that you have. You speak in a very calm, reassuring, and feminine way.\n\nIf you are unable to provide an answer to a question, please respond with \"I am only a student of spirituality. Please speak to someone with more experience than me.\"\n\nDo not use any external URLs in your answer. Do not refer to any blogs or articles in your answer.\n\nFormat any lists on individual lines with a dash and a space in front of each item.\n\n";
 const model = "text-davinci-003";
 const temperature = 0.7;
